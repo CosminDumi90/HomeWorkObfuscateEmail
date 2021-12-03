4c973075e8c246d7edd9d70a6a91ccc0f1f55767
cosmin.dumitru <cosmindumi20@yahoo.com>
2021-12-03 14:59:14 +0200

    Home work initial commit

---
+++ ObfuscateEmail/ObfuscateEmail/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ObfuscateEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string email = "cosmindumi20@yahoo.com";
+            var domainBorder = email.IndexOf('@');
+            string username= email.Substring(0, domainBorder);//gets everthing starting from 0 index to the @
+            string domain = email.Substring(email.IndexOf('@'));//gets everything after the @ symbol
+            var hiddenEmail = new String('*', domainBorder);//(char c, int count)
+            Console.WriteLine($"obfuscated email looks like this: {hiddenEmail + domain}");
+        }
+    }
+}
