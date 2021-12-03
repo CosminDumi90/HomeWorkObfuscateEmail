d582a00eaffb9efef424ccbb5f0026a1633794ca
cosmin.dumitru <cosmindumi20@yahoo.com>
2021-12-03 15:24:04 +0200

    Adding separate methods for reading user imput,validating e-mail and finally hidding the email

--- ObfuscateEmail/ObfuscateEmail/Program.cs
+++ ObfuscateEmail/ObfuscateEmail/Program.cs
@@ -6,9 +6,37 @@ namespace ObfuscateEmail
     {
         static void Main(string[] args)
         {
-            string email = "cosmindumi20@yahoo.com";
+            UserInput();
+            
+        }
+
+        static void UserInput()
+        {
+            Console.WriteLine("Please enter your e-mail adress. Exemple: someone@yahoo.com:");
+            string input = Console.ReadLine();
+            IsEmailValid(input);
+        }
+
+        static void IsEmailValid(string input)
+        {
+            if (input.Contains("@"))
+            {
+                Console.WriteLine("email is ok");
+                Obfuscate(input);
+                return;
+            }
+            else
+                Console.WriteLine("email is not ok");
+            UserInput();
+
+        }
+
+
+        static void Obfuscate(string email)
+        {
+
             var domainBorder = email.IndexOf('@');
-            string username= email.Substring(0, domainBorder);//gets everthing starting from 0 index to the @
+            string username = email.Substring(0, domainBorder);//gets everthing starting from 0 index to the @
             string domain = email.Substring(email.IndexOf('@'));//gets everything after the @ symbol
             var hiddenEmail = new String('*', domainBorder);//(char c, int count)
             Console.WriteLine($"obfuscated email looks like this: {hiddenEmail + domain}");
